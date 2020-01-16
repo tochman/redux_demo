5c0547011e0308f8c492d6985ac789ef83608de1
Thomas <thomas@craftacademy.se>
2020-01-16 14:16:28 +0100

    adds functionality to change message

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,46 @@ import { connect } from 'react-redux'
 
 class App extends Component {
 
+  changeMessage = event => {
+    event.preventDefault()
+    let newMessage = event.target.newMessage.value
+    event.target.newMessage.value = ''
+    this.props.changeMessage(newMessage)
+  }
+
   render() {
     return (
       <>
         <h1>{this.props.message}</h1>
+        <form onSubmit={this.changeMessage}>
+          <input type="text" name="newMessage" placeholder="Type a message" />
+          <input type="submit" value="Change message" />
+        </form>
+        <button
+          onClick={() => this.props.changeMessage('Hello Venus')}
+        >Change message
+        </button>
+        <button
+          onClick={() => this.props.resetMessage()}
+        >Reset
+        </button>
       </>
     );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     message: state.message
   }
 }
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = dispatch => {
+  return {
+    resetMessage: () => { dispatch({ type: 'RESET_MESSAGE' }) },
+    changeMessage: message => { dispatch({ type: 'CHANGE_MESSAGE', payload: message }) }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+// export default App
--- src/state/reducers/rootReducer.js
+++ src/state/reducers/rootReducer.js
@@ -1,7 +1,17 @@
 import initialState from '../store/initialState'
 
-const rootReducer = (state = initialState) => {
-  return state
+const rootReducer = (state = initialState, action) => {
+  if (action.type === 'CHANGE_MESSAGE') {
+    return {
+      ...state,
+      message: action.payload
+    }
+  } else if (action.type === 'RESET_MESSAGE') {
+    return initialState
+  } else {
+    return state
+  }
+
 }
 
 export default rootReducer
